28d9e3c106466f600ec4239df7910f43cf4a6d8b
Eric Feunekes <eric.feunekes@pwc.com>
2022-10-09 16:42:24 -0300

    testing new release

--- tasks.py
+++ tasks.py
@@ -5,11 +5,15 @@ from invoke import task
 def build(c, src="./blog"):
     c.run(f"jupyter-book build {src}")
 
+@task
+def docs_folder(c):
+    c.run('mkdir -p ./docs')
 
 @task
 def clean(c, src="./blog"):
     c.run(f"jupyter-book clean {src}")
 
-@task(pre=[build], post=[clean])
+@task(pre=[build, docs_folder], post=[clean])
 def release(c):
-    c.run('ghp-import -n -p -f ./blog/_build/html')
+    c.run('mv ./blog/_build/html/* ./docs')
+    c.run('touch ./docs/.nojekyll')
