7d0f32ee567b56858b83d290477e731731a39937
ericfeunekes <ericwill.f@gmail.com>
2022-10-10 20:05:49 +0000

    changing error handling

--- src/getting.py
+++ src/getting.py
@@ -2,29 +2,32 @@ from typing import Tuple
 import httpx
 import asyncio
 
+
 async def get_url(
     url: str, client: httpx.AsyncClient, limiter: asyncio.Semaphore = None
 ) -> httpx.Response:
-    '''Send and async request to the given URL using the client
-    
-    :param url: the url to get
-    :type url: str
-    :param client: the HTTPX async client to use
-    :type client: httpx.AsyncClient
-    :param limiter: a semaphore to use to limit parallel calls
-    :type limiter: asyncio.Semaphore, optional 
-    '''
-    assert isinstance(client, httpx.AsyncClient); '`client` must be an httpx.AsyncClient'
+    """Send and async request to the given URL using the client
+
+    Parameters
+    ---------
+    url : str
+        the url to get
+    """
+    assert isinstance(client, httpx.AsyncClient)
+    "`client` must be an httpx.AsyncClient"
     if not limiter:
-        return await client.get(url)
-    async with limiter:
-        return await client.get(url)
+        response = await client.get(url)
+    else:
+        async with limiter:
+            response = await client.get(url)
+    response.raise_for_status()
+    return response
+        
 
-async def get_response(response: httpx.Response, repeat:int=0) -> Tuple[bool, httpx.Response]:
-    code = response.status_code
-    match code:
-        case 200:
-            return (True, response)
-        case 429:
-            get_url
 
+async def get_response(
+    url: str, client: httpx.AsyncClient, limiter: asyncio.Semaphore = None
+) -> Tuple[bool, httpx.Response]:
+    try:
+        get_url(url=url, client=client)
+    except httpx.HTTPStatusError:
