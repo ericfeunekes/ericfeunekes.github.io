be6f2e474d93985c920f44f99ba2bd42a33e4f75
ericfeunekes <ericwill.f@gmail.com>
2022-10-11 22:17:45 +0000

    testing build for new post

--- tasks.py
+++ tasks.py
@@ -5,16 +5,18 @@ from invoke import task
 def build(c, src="./blog"):
     c.run(f"jupyter-book build {src}")
 
+
 @task
 def docs_folder(c):
-    c.run('mkdir -p ./docs')
+    c.run("mkdir -p ./docs")
+
 
 @task
 def clean(c, src="./blog"):
     c.run(f"jupyter-book clean {src}")
 
+
 @task(pre=[build, docs_folder], post=[clean])
 def release(c):
-    c.run('rm -r ./docs/*')
-    c.run('mv ./blog/_build/html/* ./docs')
-    c.run('')
+    # c.run("rm -r docs/*")
+    c.run("mv blog/_build/html/* ./docs")
--- tests/test_getting.py
+++ tests/test_getting.py
@@ -1,5 +1,5 @@
 import asyncio
-from time import time_ns  
+from time import time_ns
 import httpx
 import numpy as np
 import pytest
@@ -39,16 +39,26 @@ class TestGet_URL:
         url = f"https://httpbin.org/delay/1"
 
         times = []
+
         async def time_calls(client):
             start = time_ns()
             await get_url(url, client, limiter=sem)
             end = time_ns()
             return [start, end]
 
-
         async with httpx.AsyncClient() as client:
             todo = [time_calls(client) for i in range(10)]
             times = asyncio.gather(*todo)
         times_np = np.array(times)
-        num_calls = [len((times_np[(times_np[:,0] > times_np[i,0]) & (times_np[:,1] < times_np[i,1])])) for i in range(len(times_np))]
+        num_calls = [
+            len(
+                (
+                    times_np[
+                        (times_np[:, 0] > times_np[i, 0])
+                        & (times_np[:, 1] < times_np[i, 1])
+                    ]
+                )
+            )
+            for i in range(len(times_np))
+        ]
         assert np.max(num_calls) == 3
