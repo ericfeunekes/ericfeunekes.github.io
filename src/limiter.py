816618777768e63872d45c095a108bdd2d225cb2
ericfeunekes <ericwill.f@gmail.com>
2022-10-13 00:54:44 +0000

    checkpoint

--- src/limiter.py
+++ src/limiter.py
@@ -1,11 +1,23 @@
 from contextlib import asynccontextmanager
 from asyncio import Semaphore, sleep
+from pydantic import BaseModel, conint
 
-@asynccontextmanager
-async def Limiter(semaphore: Semaphore, period: int):
-    async with semaphore:
-        try:
-            yield
-        finally:
-            await sleep(period)
+class Limiter(BaseModel):
+    max_calls: conint(gt=0) #type: ignore
+    period: conint(gt=0) #type: ignore
+
+    class Config:
+        arbitrary_types_allowed=True
+        allow_mutation=False
+
+    
+
+
+# @asynccontextmanager
+# async def Limiter(semaphore: Semaphore, period: int):
+#     async with semaphore:
+#         try:
+#             yield
+#         finally:
+#             await sleep(period)
             
