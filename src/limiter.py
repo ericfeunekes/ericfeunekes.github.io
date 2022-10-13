7fbc7611d89845a79bf33a434a45e096b4645004
Eric Feunekes <eric.feunekes@pwc.com>
2022-10-12 22:29:20 -0300

    testing limiter

--- src/limiter.py
+++ src/limiter.py
@@ -6,18 +6,22 @@ class Limiter(BaseModel):
     max_calls: conint(gt=0) #type: ignore
     period: conint(gt=0) #type: ignore
 
+    _semaphore: Semaphore
+
     class Config:
         arbitrary_types_allowed=True
         allow_mutation=False
+        underscore_attrs_are_private=True
 
+    def __init__(self, **data):
+        super().__init__(**data)
+
+        self._semaphore = Semaphore(self.max_calls)
+
+    async def __aenter__ (self):
+        await self._semaphore.acquire()
+        return
     
-
-
-# @asynccontextmanager
-# async def Limiter(semaphore: Semaphore, period: int):
-#     async with semaphore:
-#         try:
-#             yield
-#         finally:
-#             await sleep(period)
-            
+    async def __aexit__ (self, exc_type, exc, tb):
+        await sleep(self.period)
+        self._semaphore.release()
--- tests/test_limiter.py
+++ tests/test_limiter.py
@@ -1,26 +1,33 @@
 import asyncio
-from asyncio import Semaphore
 from datetime import datetime
 import numpy as np
 import pytest
 
 from src.limiter import Limiter
 
+def test_init_limiter():
+    print('start')
+    limiter = Limiter(max_calls=3, period=1)
+    assert limiter.max_calls == 3
+    assert limiter.period == 1
+    assert limiter._semaphore is not None
+
 @pytest.mark.asyncio
-async def test_init_limiter():
+async def test_limiter():
     num_calls = 4
-    sem = Semaphore(num_calls)
+    limiter = Limiter(max_calls=num_calls, period=2)
     call_times = []
+
     async def mark_time(i: int, limiter: Limiter):
         async with limiter:
-            return {'call':i, 'time':datetime.utcnow()}
-    
-    tasks = [mark_time(i, Limiter(sem, 2)) for i in range(8)]
+            return {"call": i, "time": datetime.utcnow()}
+
+    tasks = [mark_time(i, limiter) for i in range(8)]
     times = await asyncio.gather(*tasks)
 
-    first_batch = [t['time'].second for t in times[:num_calls]]
+    first_batch = [t["time"].second for t in times[:num_calls]]
     first_val = first_batch[0]
-    np.testing.assert_equal(first_batch, [first_val]*num_calls)
-    second_batch = [t['time'].second for t in times[num_calls:]]
+    np.testing.assert_equal(first_batch, [first_val] * num_calls)
+    second_batch = [t["time"].second for t in times[num_calls:]]
     second_val = second_batch[0]
-    np.testing.assert_equal(second_batch, [second_val]*num_calls)
+    np.testing.assert_equal(second_batch, [second_val] * num_calls)
